f890ae8060d5e9f7d9fea77d76b6bbd8d90da183
misterwolf <dario.gas@gmail.com>
2017-09-26 17:14:32 +0200

    First version

---
+++ console/migrations/m170926_151122_create_forum.php
@@ -0,0 +1,33 @@
+<?php
+
+use yii\db\Migration;
+
+class m170926_151122_create_forum extends Migration
+{
+    public function safeUp()
+    {
+
+    }
+
+    public function safeDown()
+    {
+        echo "m170926_151122_create_forum cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m170926_151122_create_forum cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
